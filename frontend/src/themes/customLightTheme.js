940e3d47e65ed28476c4d6aac4c784c02de37d32
Артём <artyom25kakayev@mail.ru>
2022-01-16 11:38:20 +0300

    Frontend design - enable custom theme

---
+++ frontend/src/themes/customLightTheme.js
@@ -0,0 +1,29 @@
+import { defaultTheme } from 'react-admin';
+import merge from 'lodash/merge';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
+
+export const customLightTheme = merge({}, defaultTheme, {
+    palette: {
+        primary: indigo,
+        secondary: pink,
+        error: red,
+        contrastThreshold: 3,
+        tonalOffset: 0.2,
+    },
+    typography: {
+        // Use the system font instead of the default Roboto font.
+        fontFamily: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Arial', 'sans-serif'].join(','),
+    },
+    overrides: {
+        MuiButton: { // override the styles of all instances of this component
+            root: { // Name of the rule
+                color: 'black', // Some CSS
+            },
+            textPrimary: {
+                color: 'black'
+            }
+        },
+    },
+});
